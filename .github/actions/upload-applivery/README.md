1113337219e0fbc1f144c82c09231630e138db14
Yingying Chen <ychen@clicktherapeutics.com>
2023-06-22 11:28:56 +0800

    feat: update workflow

--- .github/actions/upload-applivery/README.md
+++ .github/actions/upload-applivery/README.md
@@ -15,6 +15,6 @@ steps:
       api-key: ${{ env.APPLIVERY_API_KEY }}
       app-file: ${{ env.ARTIFACT_PATH }}
       tags: 'production,english'
-      release-notes: 'some bug fixes'
-      notifyCollaborators: true
+      changelog: '05_08_23 PR 2707 Commit fd51f5b v0.1.0 Alpha KDV13'
+      NOTIFY: true
 ```
--- .github/actions/upload-applivery/upload-applivery.sh
+++ .github/actions/upload-applivery/upload-applivery.sh
@@ -3,6 +3,6 @@ curl https://api.applivery.io/v1/integrations/builds \
     -X POST \
     -H "Authorization: bearer $API_KEY" \
     -F build="@$APP_FILE" \
-    -F notify="$NOTIFY" \
-    -F release_notes="$RELEASE_NOTES" \
+    -F notifyCollaborators="$NOTIFY" \
+    -F changelog="$CHANGELOG" \
     -F tags="$TAGS"
