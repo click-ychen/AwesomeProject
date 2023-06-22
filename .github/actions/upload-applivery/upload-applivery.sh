84860dd74173fb00e3e5d711cc62c0d5ffb4e838
Yingying Chen <ychen@clicktherapeutics.com>
2023-06-22 11:15:04 +0800

    feat: update workflow

--- .github/actions/upload-applivery/upload-applivery.sh
+++ .github/actions/upload-applivery/upload-applivery.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 curl https://api.applivery.io/v1/integrations/builds \
-    -F api_key="bearer $API_KEY" \
-    -F file="@$APP_FILE" \
+    -X POST \
+    -H "Authorization: bearer $API_KEY" \
+    -F build="@$APP_FILE" \
     -F notify="$NOTIFY" \
     -F release_notes="$RELEASE_NOTES" \
     -F tags="$TAGS"
